9818dd73ec8dfc2b4bc4f9295b10483b68221f26
Monica J <jacob.jessup@jointheleague.org>
2021-11-03 17:28:28 -0700

    Trying a new API, Feature 3(WIP)

---
+++ src/main/java/org/jointheleague/features/student/NasaPictureOfTheDay.java
@@ -0,0 +1,95 @@
+package org.jointheleague.features.student;
+
+import org.javacord.api.event.message.MessageCreateEvent;
+import org.jointheleague.features.abstract_classes.Feature;
+import org.jointheleague.features.help_embed.plain_old_java_objects.help_embed.HelpEmbed;
+import org.jointheleague.features.student.NasaPicture.NasaPictureWrapper;
+import org.springframework.web.reactive.function.client.WebClient;
+
+
+import reactor.core.publisher.Mono;
+
+public class NasaPictureOfTheDay extends Feature{
+	public final String COMMAND = "!nasaAPOD";
+
+    private WebClient webClient;
+    private static final String baseUrl = "https://api.nasa.gov/planetary/apod";
+    private final String apiKey = "Y01ELGRKSZuaczdrx5m4KJc3sf93yOh5jywRR51U";
+
+    public NasaPictureOfTheDay(String channelName) {
+        super(channelName);
+        helpEmbed = new HelpEmbed(COMMAND, "explanation");
+
+        //build the WebClient
+        this.webClient = WebClient
+                .builder()
+                .baseUrl(baseUrl)
+                .build();
+    }
+
+    @Override
+    public void handle(MessageCreateEvent event) {
+        String messageContent = event.getMessageContent();
+        if (messageContent.startsWith(COMMAND)) {
+            messageContent = messageContent
+                    .replace(COMMAND, "")
+                    .replace(" " , "");
+            
+            
+                String pictureInfo = findPicture();
+                event.getChannel().sendMessage(pictureInfo);
+            
+        }
+    }
+
+    public NasaPictureWrapper getSomethingSomething() {
+        Mono<NasaPictureWrapper> apiExampleWrapperMono = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("api_key", apiKey)
+                        .build())
+                .retrieve()
+                .bodyToMono(NasaPictureWrapper.class);
+
+        return apiExampleWrapperMono.block();
+    }
+
+    public String findPicture() {
+    	NasaPictureWrapper nasaPictureWrapper = getSomethingSomething();
+    	String title = nasaPictureWrapper.getTitle();
+    	
+    	
+    	
+    	return title;
+    }
+    
+//    public String findStory(String topic){
+//
+//        //Get a story from News API
+//        ApiExampleWrapper apiExampleWrapper = getNewsStoryByTopic(topic);
+//
+//        //Get the first article
+//        Article article = apiExampleWrapper.getArticles().get(0);
+//
+//        //Get the title of the article
+//        String articleTitle = article.getTitle();
+//
+//        //Get the content of the article
+//        String articleContent = article.getContent();
+//
+//        //Get the URL of the article
+//        String articleUrl = article.getUrl();
+//
+//        //Create the message
+//        String message =
+//                articleTitle + " -\n"
+//                        + articleContent
+//                        + "\nFull article: " + articleUrl;
+//
+//        //Send the message
+//        return message;
+//    }
+
+    public void setWebClient(WebClient webClient) {
+        this.webClient = webClient;
+    }
+}
