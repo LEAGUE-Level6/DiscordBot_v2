e4509f9228cce7ff7dcc596b2aecbe297a54f887
Cody <cody.sauer@jointheleague.org>
2021-11-09 16:13:12 -0500

    Added fixes to builder and request to pass bearer auth and # on path. You may need to set up an API_KEY environment variable or use your own token to get it to work. This API is IP locked.

--- src/main/java/org/jointheleague/features/student/feature3.java
+++ src/main/java/org/jointheleague/features/student/feature3.java
@@ -2,37 +2,19 @@ package org.jointheleague.features.student;
 
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.jointheleague.features.abstract_classes.Feature;
-import org.jointheleague.features.examples.third_features.plain_old_java_objects.news_api.ApiExampleWrapper;
-import org.jointheleague.features.examples.third_features.plain_old_java_objects.news_api.Article;
 import org.jointheleague.features.help_embed.plain_old_java_objects.help_embed.HelpEmbed;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-import sun.nio.cs.UTF_8;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URLEncoder;
 
 
 public class feature3 extends Feature{
         public final String COMMAND = "!player";
 
         private WebClient webClient;
-        private static String baseUrl;
+        private static String baseUrl = "https://api.clashroyale.com/v1/players";
 
-    static {
-        try {
-            baseUrl = new URI("https://api.clashroyale.com/v1/players/#9JJCYYRP8").toString();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private final String apiKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQi" +
-                "OiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImZmNzRhYmEzLTRmYzctNDU2OS05N2I2LTY0NTIwZmFjZGIxZCIsImlhdCI6MTYzMzkxMDE1NSwic3ViIjoiZGV2ZWxvcGVyL2Q4ZTNmNmEz" +
-                "LThhMGItZDAwMS01NDJkLWUwYzAzOGI0ZTU3OSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWR" +
-                "ycyI6WyI3Ni45MS4xOTkuNDYiXSwidHlwZSI6ImNsaWVudCJ9XX0.Gsi7OAvKxu6XO83_uX5lI7edy6ZQ6wNA0tE2Xu_azuJNUTt_XUKMkzxfp9a5lWND8xjvklMFrisibhWE2CGybw";
-
+    private final String apiKey = System.getenv("API_KEY");
+    ;
         public feature3(String channelName) {
             super(channelName);
             helpEmbed = new HelpEmbed(COMMAND, "Example of using an API to get information from another service.  This returns a news story related to a topic (e.g. !apiExample cats)");
@@ -40,6 +22,7 @@ public class feature3 extends Feature{
             //build the WebClient
             this.webClient = WebClient
                     .builder()
+                    .defaultHeaders(headers -> headers.setBearerAuth(apiKey))
                     .baseUrl(baseUrl)
                     .build();
         }
@@ -50,38 +33,22 @@ public class feature3 extends Feature{
             if (messageContent.startsWith(COMMAND)) {
                 messageContent = messageContent
                         .replace(COMMAND, "")
-                        .replace(" " , "");
-                if (messageContent.equals("")) {
-                    event.getChannel().sendMessage("Please put your user ID after the command (do not include #");
-                }
-                else{
-                   // String story = findPlayer(messageContent);
-                   // event.getChannel().sendMessage(story);
-                    getUserId(messageContent);
-                }
-
-
-
+                        .replace(" ", "");
+                getUserId(messageContent);
             }
+
+
         }
 
         public void getUserId(String id) {
-           /* System.out.println(baseUrl);
-            System.out.println(id);
-            this.webClient = WebClient
-                    .builder()
-                    .baseUrl(baseUrl + id.trim())
-                    .build(); */
-            String apiExampleWrapperMono = webClient.get()
-                    .headers(headers -> headers.setBearerAuth(apiKey))
-                            .retrieve()
-                            .bodyToMono(String.class)
-                            .block();
 
-
-
-System.out.println(apiExampleWrapperMono);
-
+            Mono<String> test = webClient.get()
+                    .uri(uriBuilder -> uriBuilder
+                            .path("/#9JJCYYRP8")
+                            .build())
+                    .retrieve()
+                    .bodyToMono(String.class);
+            System.out.print(test.block());
         }
 
      /*   public String findPlayer(String topic){
