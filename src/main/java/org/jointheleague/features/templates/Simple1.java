b02db72ae265d9db3129185c110ebd30d9556a63
jbear18 <theyodelingcowgirl24@gmail.com>
2022-01-12 19:11:46 -0800

    first commit

---
+++ src/main/java/org/jointheleague/features/templates/Simple1.java
@@ -0,0 +1,26 @@
+package org.jointheleague.features.templates;
+
+import java.util.Random;
+
+import org.javacord.api.event.message.MessageCreateEvent;
+import org.jointheleague.features.abstract_classes.Feature;
+import org.jointheleague.features.help_embed.plain_old_java_objects.help_embed.HelpEmbed;
+
+public class Simple1 extends Feature {
+
+    public final String COMMAND = "!greeting";
+
+    public Simple1(String channelName) {
+        super(channelName);
+        helpEmbed = new HelpEmbed(COMMAND, "Greets you");
+    }
+
+    @Override
+    public void handle(MessageCreateEvent event) {
+        String messageContent = event.getMessageContent();
+        if (messageContent.startsWith(COMMAND)) {
+            event.getChannel().sendMessage("Hello!");
+        }
+    }
+
+}
