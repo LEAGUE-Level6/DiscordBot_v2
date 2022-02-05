d287e15998b101f20fa27032c5639542b3a99f04
root <root@b6dd2641afe8>
2022-02-05 02:25:44 +0000

    Working on feature 2 - interpreting the user's guess on
    the animal game and determining if it's correct or not. Add guesses?

--- src/main/java/feature2/Nathan/Feature2.java
+++ src/main/java/feature2/Nathan/Feature2.java
@@ -1,13 +1,19 @@
 package feature2.Nathan;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.jointheleague.features.abstract_classes.Feature;
 import org.jointheleague.features.help_embed.plain_old_java_objects.help_embed.HelpEmbed;
 
 public class Feature2 extends Feature {
 
-    public final String COMMAND = "!story";
-
+    public final String COMMAND = "!animal";
+    Random r=new Random();
+    int animalNumber;
+ 
     public Feature2(String channelName) {
         super(channelName);
 
@@ -21,10 +27,63 @@ public class Feature2 extends Feature {
     @Override
     public void handle(MessageCreateEvent event) {
         String messageContent = event.getMessageContent();
-        if (messageContent.startsWith(COMMAND)) {
             //respond to message here
-            event.getChannel().sendMessage("Sending a message to the channel");
+        	//r.nextInt(10);
+        	ArrayList<Integer> animalNumbers=new ArrayList<Integer>();
+        	for(int i=0; i<10; i++) {
+        		animalNumber=r.nextInt(10);
+        		animalNumbers.add(animalNumber);
+        	}
+        	
+        	for(int j=0; j<10; j++) {
+        		if (messageContent.startsWith(COMMAND)) {
+        			
+        	if(animalNumbers.get(j)==0) {
+            event.getChannel().sendMessage("Long legs, short bodies, bony horns");
+            String guessMessage = messageContent.replaceAll(" ", "").replace(COMMAND, "");
+            if(guessMessage=="giraffe" || guessMessage=="Giraffe") {
+                event.getChannel().sendMessage("Correct!");     
+            }
+        	}
+        	
+        	if(animalNumbers.get(j)==1) {
+        		//monkey
+        		event.getChannel().sendMessage("forward facing eyes, grasping hands, nails, large brains");
+        	}
+        	if(animalNumbers.get(j)==2) {
+        		//alligator
+        		event.getChannel().sendMessage("long body, thick scales, short legs");
+        	}
+        	if(animalNumbers.get(j)==3) {
+        		//panda
+        		event.getChannel().sendMessage("black fur, black ears, white head");
+        	}
+        	if(animalNumbers.get(j)==4) {
+        		//elephant
+        		event.getChannel().sendMessage("big head, wide ears, think skin");
+        	}
+        	if(animalNumbers.get(j)==5) {
+        		//whale
+        		event.getChannel().sendMessage("long body, blue-gray colored skin");
+        	}
+        	if(animalNumbers.get(j)==6) {
+        		//tiger
+        		event.getChannel().sendMessage("powerful carnivore, sharp teeth, agile body");
+        	}
+        	if(animalNumbers.get(j)==7) {
+        		//llama
+        		event.getChannel().sendMessage("long neck, thick fur, split upper lip");
+        	}
+        	if(animalNumbers.get(j)==8) {
+        		//kangaroo
+        		event.getChannel().sendMessage("muscular tail, large feet, long pointed ears");
+        	}
+        	if(animalNumbers.get(j)==9) {
+        		//bear
+        		event.getChannel().sendMessage("large bodies, stocky legs, short tail, shaggy hair");
+        	}
         }
+    }
     }
 
 }
