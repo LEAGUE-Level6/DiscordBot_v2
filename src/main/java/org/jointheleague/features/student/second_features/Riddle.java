50b8b1d2825fe14b10e9284dc31a766ec4311a7e
dylanvanwinkle <dylanvanwinkle@gmail.com>
2022-03-15 19:48:59 -0700

    Riddle

--- src/main/java/org/jointheleague/features/student/second_features/Riddle.java
+++ src/main/java/org/jointheleague/features/student/second_features/Riddle.java
@@ -4,13 +4,22 @@ import org.javacord.api.event.message.MessageCreateEvent;
 import org.jointheleague.features.abstract_classes.Feature;
 import org.jointheleague.features.help_embed.plain_old_java_objects.help_embed.HelpEmbed;
 
+import java.util.HashMap;
 import java.util.Random;
 
 public class Riddle extends Feature {
 
     public final String COMMAND = "!riddle";
     private final Random random = new Random();
-    private final String answer = "An egg";
+    private final String answer = "egg";
+    String[] answers = { "egg", "candle", "all of them" };
+    String[] questions = { "What has to be broken before you can use it?", " I’m tall when I’m young, and I’m short when I’m old. What am I?", "What month of the year has 28 days?" };
+    String COMMAND2;
+    int ran;
+    public void randomCreater(){
+        ran = random.nextInt(3);
+        COMMAND2 = "!riddle " + answers[ran];
+    }
 
     public Riddle(String channelName) {
         super(channelName);
@@ -22,20 +31,14 @@ public class Riddle extends Feature {
         String messageContent = event.getMessageContent();
 
         //start the game with the command
-        if (messageContent.equals(COMMAND)) {
-            event.getChannel().sendMessage("What has to be broken before you can use it?");
+        if (messageContent.equalsIgnoreCase((COMMAND))){
+            randomCreater();
+            event.getChannel().sendMessage(questions[ran]);
         }
         //check a guess
-        else if (messageContent.contains(COMMAND)
-                && !messageContent.contains("e.g.")
-                && !messageContent.contains("this:")) {
-
-            //parse the guess from the message
-            String guessMessage = messageContent.replaceAll(" ", "").replace(COMMAND, "");
-
+        if (messageContent.equalsIgnoreCase(COMMAND2)) {
+            event.getChannel().sendMessage("Correct! The answer was " + answers[ran] + ".");
 
         }
-            event.getChannel().sendMessage("Correct!  The answer was " + answer);
         }
     }
-}
