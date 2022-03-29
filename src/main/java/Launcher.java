806ff2228bf70f84b6cd6c017376e1789b0c9f9e
League Student <league@iMac-13.attlocal.net>
2022-03-28 18:28:08 -0700

    Test

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,7 +1,8 @@
 import org.jointheleague.discord_bot.DiscordBot;
-
+import org.jointheleague.discord_bot.DiscordBotOld;
 public class Launcher {
     public static void main(String[] args) {
-        new DiscordBot(System.getenv("DISCORD_TOKEN")).Connect();
+        //new DiscordBotOld(System.getenv("DISCORD_TOKEN")).Connect();
+    	new DiscordBot(System.getenv("DISCORD_TOKEN")).Connect();
     }
 }
--- src/main/java/org/jointheleague/discord_bot/DiscordBot.java
+++ src/main/java/org/jointheleague/discord_bot/DiscordBot.java
@@ -1,85 +1,54 @@
 package org.jointheleague.discord_bot;
-import java.util.HashMap;
-import java.util.Random;
-import java.util.concurrent.ExecutionException;
-
-import org.javacord.api.entity.message.component.ActionRow;
-import org.javacord.api.entity.message.component.Button;
-import org.javacord.api.interaction.MessageComponentInteraction;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import javax.swing.Timer;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.MessageBuilder;
-import org.javacord.api.listener.message.MessageCreateListener;
-import org.javacord.api.util.event.ListenerManager;
+class Game implements ActionListener {
+	Message Message;
+	TextChannel Channel;
+	MessageBuilder Builder;
+	BufferedImage Image;
+	Graphics2D Canvas;
+	int X;
+	Game(Message Message) {
+		this.Message = Message;
+		this.Channel = this.Message.getChannel();
+		this.Image = new BufferedImage(1000, 500, BufferedImage.TYPE_INT_RGB);
+		this.Canvas = this.Image.createGraphics();
+		new Timer(1000, this).start();
+	}
+	@Override
+	public void actionPerformed(ActionEvent Event) {
+		X++;
+		this.Canvas.setColor(Color.BLACK);
+		this.Canvas.fillRect(0, 0, 1000, 500);
+		this.Canvas.setColor(Color.RED);
+		this.Canvas.fillRect(X * 10, 10, 10, 10);
+		this.Builder = new MessageBuilder();
+		this.Builder.addAttachment(this.Image, "Frame.png");
+		this.Message.delete();
+		this.Builder.send(this.Channel);
+	}
+}
 public class DiscordBot {
 	DiscordApi API;
 	public DiscordBot(String Token) {
 		this.API = new DiscordApiBuilder().setToken(Token).login().join();
 	}
-	HashMap<Long, ListenerManager<MessageCreateListener>> AnnoyedChannels = new HashMap<Long, ListenerManager<MessageCreateListener>>();
-	HashMap<Long, String> AnnoyedChannelStarters = new HashMap<Long, String>();
-	Random Rand = new Random();
 	public void Connect() {
-		long ClientID = API.getClientId();
-		API.addMessageCreateListener(E -> {
-			String Content = E.getMessageContent();
-			if (Content.length() > 6) {
-				String[] Data = Content.substring(6).split(" ");
-				TextChannel Channel = E.getChannel();
-				Long ChannelID = Channel.getId();
-				String AuthorName = E.getMessageAuthor().asUser().get().getName();
-				if (Content.startsWith("BBot: ")) {
-					switch (Data[0]) {
-						case "Annoy":
-							if (!AnnoyedChannels.containsKey(ChannelID)) {
-								Channel.sendMessage("Done");
-								AnnoyedChannels.put(ChannelID, Channel.addMessageCreateListener(M -> {
-									if (M.getMessageAuthor().getId() != ClientID) {
-										Channel.sendMessage("\"" + M.getMessageContent() + "\"");
-									}
-								}));
-								AnnoyedChannelStarters.put(ChannelID, AuthorName);
-							} else {
-								Channel.sendMessage("Already active");
-							}
-							break;
-						case "StopAnnoy":
-							if (!AnnoyedChannels.containsKey(ChannelID)) {
-								Channel.sendMessage("Not active");
-							} else if (!AnnoyedChannelStarters.get(ChannelID).equals(AuthorName)) {
-								Channel.sendMessage("No");
-							} else {
-								Channel.sendMessage("Done");
-								AnnoyedChannels.get(ChannelID).remove();
-								AnnoyedChannels.remove(ChannelID);
-								AnnoyedChannelStarters.remove(ChannelID);
-							}
-							break;
-						case "RPS":
-							try {
-								Message M = new MessageBuilder().setContent("Choose one").addComponents(ActionRow.of(Button.success("Rock", "Rock"), Button.success("Paper", "Paper"), Button.success("Scissors", "Scissors"))).send(Channel).get();
-								API.addMessageComponentCreateListener(S -> {
-									MessageComponentInteraction Interaction = S.getMessageComponentInteraction();
-									int Result = Rand.nextInt(100);
-									if (Result < 75) {
-										Interaction.createImmediateResponder().setContent("You lost!").respond();
-									} else if (Result < 90) {
-										Interaction.createImmediateResponder().setContent("Tie!").respond();
-									} else {
-										Interaction.createImmediateResponder().setContent("You won!").respond();
-									}
-									M.delete();
-								});
-							} catch(Exception S) {	
-							}
-							break;
-						
-						default:
-							Channel.sendMessage("\"BBot: Help\" for help");
-							break;
-					}
+		this.API.addMessageCreateListener(Message -> {
+			if (Message.getMessageContent().equals("BBot: Test")) {
+				try {
+					new Game(Message.getChannel().sendMessage("Loading...").get());
+				} catch (Exception E) {
+					E.printStackTrace();
 				}
 			}
 		});
