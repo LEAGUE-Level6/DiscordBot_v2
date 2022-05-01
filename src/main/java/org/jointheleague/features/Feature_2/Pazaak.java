42578ced5d163a74d9d42d7a609d3e508909298c
Jacob Handy <jacob.handy@jointheleague.org>
2022-05-01 11:31:07 -0700

    Continued development of Pazaak, just need to finish bot logic and unit tests

--- src/main/java/org/jointheleague/features/Feature_2/Pazaak.java
+++ src/main/java/org/jointheleague/features/Feature_2/Pazaak.java
@@ -12,6 +12,10 @@ public class Pazaak extends Feature {
     public final String COMMAND = "!Pazaak";
     public ArrayList<Integer> sideDeck;
     public ArrayList<Integer> mainDeck;
+    public ArrayList<Integer> playerHand;
+    public boolean playerStand = false;
+    public boolean botStand = false;
+    public int cardIndex = 0;
     public Pazaak(String channelName) {
         super(channelName);
 
@@ -41,7 +45,84 @@ public class Pazaak extends Feature {
             int turnCt = 1;
             int playerTot = 0;
             int botTot = 0;
+            int playerWins = 0;
+            int botWins = 0;
+            //Deal hand; this stays the same throughout the match
+            dealHand();
+            //Display player hand
+            event.getChannel().sendMessage("Your hand: " + playerHand.get(0) + " " +
+                    playerHand.get(1) + " " + playerHand.get(2) + " " + playerHand.get(3) + " ");
+            while(playerWins < 3 || botWins < 3) {
+                //Turn sequence:
 
+               //PLAYER TURN:
+                if (turnCt == 1) {
+                    event.getChannel().sendMessage("Your turn!");
+                    //Deal a card to the player
+                    playerTot += mainDeck.get(cardIndex);
+                    event.getChannel().sendMessage("Your card: " + mainDeck.get(cardIndex));
+                    event.getChannel().sendMessage("Your total: " + playerTot);
+                    cardIndex++;
+                    //Ask if they want to play a card ("N" or "Y card value)
+                    event.getChannel().sendMessage("Do you want to play a card? Type only the card value you want to play" +
+                            "eg: '10' or '-2', otherwise you won't be playing anything");
+                    if (messageContent.startsWith("N")) {
+                        continue;
+                    } else {
+                        try {
+                            int playerInput = Integer.parseInt(messageContent);
+                            boolean isValid = false;
+                            //check if the input is a card in the player's hand
+                            for (int i = 0; i < playerHand.size(); i++) {
+                                if (playerInput == playerHand.get(i)) {
+                                    isValid = true;
+                                    //remove card from their hand
+                                    playerHand.remove(i);
+                                    break;
+                                }
+                            }
+                            if (isValid) {
+                                //Add their card to their total
+                                playerTot += playerInput;
+                                event.getChannel().sendMessage("The card you played: " + playerInput);
+                                event.getChannel().sendMessage("Your total: " + playerTot);
+                            } else {
+                                event.getChannel().sendMessage("Play a card in your hand, idiot");
+                            }
+                        } catch (Exception e) {
+                            event.getChannel().sendMessage("Enter either 'N' or the value of the card you want to play");
+                        }
+                    }
+                    //Ask if they want to stand
+                    event.getChannel().sendMessage("Do you want to stand? (type 'y' or 'n')");
+                    if (event.getMessageContent().startsWith("y")) {
+                        playerStand = true;
+                        event.getChannel().sendMessage("Successfully stand-ed :)");
+                    } else if (event.getMessageContent().startsWith("n")) {
+                        event.getChannel().sendMessage("Successfully not stand-ed!");
+                    } else {
+                        event.getChannel().sendMessage("Enter y or n, you Gungan. I'm just going to assume you don't want to stand.");
+                    }
+                    turnCt *= -1;
+                }
+
+                //BOT TURN:
+                if(turnCt == -1){
+                    event.getChannel().sendMessage("My turn!");
+                    //Pretty easy, just deal a card to their total
+                    botTot += mainDeck.get(cardIndex);
+                    event.getChannel().sendMessage("My total: " + botTot);
+                    //randomly determine if they will use a card from their side deck (the bot is actually cheating :O )
+                    Random r = new Random();
+                    int x = r.nextInt(5);
+                    if(x == 1){
+                        int card = r.nextInt(6);
+                        botTot += card;
+                        event.getChannel().sendMessage("My total: " + botTot);
+                    }
+                    //use logic to determine if the bot should stand
+                }
+            }
         }
     }
 
@@ -73,5 +154,29 @@ public class Pazaak extends Feature {
             mainDeck.set(n1, n2);
             mainDeck.set(n2, parser);
         }
+        //shuffle side decks
+        for(int i = 0; i < shuffleTimes; i++){
+            //get random indexes in the deck
+            Random r = new Random();
+            int n1 = r.nextInt(10);
+            int n2 = r.nextInt(10);
+            int parser = sideDeck.get(n1);
+            //swap the indexes
+            sideDeck.set(n1, n2);
+            sideDeck.set(n2, parser);
+        }
+    }
+    public void dealHand(){
+        //Create arraylist of 4 cards
+        playerHand = new ArrayList<Integer>();
+        for(int i = 0; i < 4; i++){
+            playerHand.add(sideDeck.get(i));
+        }
+    }
+    public void PlayerTurn(){
+        //deal a card
+    }
+    public void EnemyTurn(){
+
     }
 }
