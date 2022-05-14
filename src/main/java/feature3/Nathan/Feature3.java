25f8aa62af1f872b3c418cba7f25889c2b6edecd
Nathan Young <nathaneric07@icloud.com>
2022-05-14 21:25:44 +0000

    Working on coming up with an idea for my game (don't use apis)

--- src/main/java/feature3/Nathan/Feature3.java
+++ src/main/java/feature3/Nathan/Feature3.java
@@ -2,31 +2,20 @@ package feature3.Nathan;
 
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.jointheleague.features.abstract_classes.Feature;
-import org.jointheleague.features.examples.third_features.plain_old_java_objects.cat_facts_api.CatWrapper;
 import org.jointheleague.features.help_embed.plain_old_java_objects.help_embed.HelpEmbed;
-import org.springframework.web.reactive.function.client.WebClient;
 
-import reactor.core.publisher.Mono;
+public class Feature3 extends Feature {
 
-public class Feature3 extends Feature{
-	public final String COMMAND = "!foodimage";
+    public final String COMMAND = "!startgame";
 
-	private WebClient webClient;
-    private static final String baseUrl = "https://foodish-api.herokuapp.com/api/";
-    
     public Feature3(String channelName) {
         super(channelName);
 
         //Create a help embed to describe feature when !help command is sent
         helpEmbed = new HelpEmbed(
                 COMMAND,
-                "When the command !foodimage is entered, an image of a food item will appear and the user will guess what dish or food category it is."
+                "Give a brief description of your feature here, including how the user interacts with it"
         );
-        
-        this.webClient = WebClient
-                .builder()
-                .baseUrl(baseUrl)
-                .build();
     }
 
     @Override
@@ -34,34 +23,8 @@ public class Feature3 extends Feature{
         String messageContent = event.getMessageContent();
         if (messageContent.startsWith(COMMAND)) {
             //respond to message here
-        	String foodURL=getCatFact();
-            event.getChannel().sendMessage(foodURL);
+            event.getChannel().sendMessage("Sending a message to the channel");
         }
     }
-    
-    public String getCatFact() {
 
-        //Make the request, accepting the response as a plain old java object you created
-        Mono<CatWrapper> catWrapperMono = webClient.get()
-                .retrieve()
-                .bodyToMono(CatWrapper.class);
-
-        //collect the response into a plain old java object
-        CatWrapper catWrapper = catWrapperMono.block();
-
-        //get the cat fact from the response
-        String message = catWrapper.getData().get(0);
-
-        //send the message
-        return message;
-    }
-
-    public void setWebClient(WebClient webClient) {
-        this.webClient = webClient;
-    }
-    
-    public void dataInterpretation(){
-    	String dish="dessert";
-    	
-    }
 }
