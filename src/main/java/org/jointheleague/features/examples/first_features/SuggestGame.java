c432feb713519ad60b379be792f8a6e50a82bbb1
YourName <leagueStudent>
2022-07-28 18:31:40 -0700

    1st feature (suggest game) (ip)
    Everything's written out, I'm just debugging at the moment

---
+++ src/main/java/org/jointheleague/features/examples/first_features/SuggestGame.java
@@ -0,0 +1,32 @@
+package org.jointheleague.features.examples.first_features;
+
+import java.util.Random;
+
+import org.javacord.api.event.message.MessageCreateEvent;
+import org.jointheleague.features.abstract_classes.Feature;
+import org.jointheleague.features.help_embed.plain_old_java_objects.help_embed.HelpEmbed;
+
+import net.aksingh.owmjapis.api.APIException;
+
+public class SuggestGame extends Feature {
+
+	public final String COMMAND = "!suggestion";
+	public final String EXPLANATION = "Suggests an online game to play";
+	public final String[] GAMES = {"https://www.nytimes.com/games/wordle/index.html", "https://wafflegame.net/", "https://offline-dino-game.firebaseapp.com/", "https://www.google.com/doodles/valentines-day-2017-day-1"};
+	
+	public SuggestGame(String channelName) {
+		super(channelName);
+		helpEmbed = new HelpEmbed(COMMAND, EXPLANATION);
+	}
+
+	@Override
+	public void handle(MessageCreateEvent event) throws APIException {
+		// TODO Auto-generated method stub
+		String message = event.getMessageContent();
+		if (message.startsWith(COMMAND)) {
+            Random r = new Random();
+            //event.getChannel().sendMessage(GAMES[r.nextInt(4)]);
+            event.getChannel().sendMessage("wrews");
+        }
+	}
+}
