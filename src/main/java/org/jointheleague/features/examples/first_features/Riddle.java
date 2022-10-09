5558c122408e7e7e5e462b38c4a4be6571891c42
AaronD07 <38016960+AaronD07@users.noreply.github.com>
2022-10-09 13:01:44 -0700

    Update Riddle.java

--- src/main/java/org/jointheleague/features/examples/first_features/Riddle.java
+++ src/main/java/org/jointheleague/features/examples/first_features/Riddle.java
@@ -32,18 +32,28 @@ public class Riddle extends Feature implements ActionListener {
     String j= "The name for the equation: PV=nRT";
     String j1= "Ideal Gas Law";
     public final String COMMAND1 = "!riddle1";
+    public final String COMMAND1A = "!answer1";
     public final String COMMAND2 = "!riddle2";
+    public final String COMMAND2A = "!answer2";
     public final String COMMAND3 = "!riddle3";
+    public final String COMMAND3A = "!answer3";
     public final String COMMAND4 = "!riddle4";
+    public final String COMMAND4A = "!answer4";
     public final String COMMAND5 = "!riddle5";
+    public final String COMMAND5A = "!answer5";
     public final String COMMAND6 = "!riddle6";
+    public final String COMMAND6A = "!answer6";
     public final String COMMAND7 = "!riddle7";
+    public final String COMMAND7A = "!answer7";
     public final String COMMAND8 = "!riddle8";
+    public final String COMMAND8A = "!answer8";
     public final String COMMAND9 = "!riddle9";
+    public final String COMMAND9A = "!answer9";
     public final String COMMAND0 = "!riddle0";
+    public final String COMMAND0A = "!answer0";
     public final String SCORE = "!score";
     int score=0;
-    Timer qTimer = new Timer(3000, this);
+    Timer qTimer = new Timer(6000, this);
     public Riddle(String channelName ) {
         super(channelName);
     }
@@ -53,198 +63,213 @@ public class Riddle extends Feature implements ActionListener {
     public void handle(MessageCreateEvent event) throws APIException, InterruptedException {
 
      if(event.getMessageContent().contains(COMMAND1)) {
-         event.getChannel().sendMessage(question1(event));
-     } else if(event.getMessageContent().contains(COMMAND2)) {
-         event.getChannel().sendMessage(question2(event));
+         event.getChannel().sendMessage(a);
+     }
+     else if(event.getMessageContent().contains(COMMAND1A)){
+            question1(event);
+        }
+     else if(event.getMessageContent().contains(COMMAND2)) {
+         event.getChannel().sendMessage(b);
+     }
+     else if(event.getMessageContent().contains(COMMAND2A)) {
+         question2(event);
      }
      else if(event.getMessageContent().contains(COMMAND3)) {
-         event.getChannel().sendMessage(question3(event));
+         event.getChannel().sendMessage(c);
      }
-     else if(event.getMessageContent().contains(COMMAND4)) {
-         event.getChannel().sendMessage(question4(event));
+     else if(event.getMessageContent().contains(COMMAND3A)) {
+         question3(event);
      }
-     else if(event.getMessageContent().contains(COMMAND5)) {
-         event.getChannel().sendMessage(question5(event));
-     }
-     else if(event.getMessageContent().contains(COMMAND6)) {
-         event.getChannel().sendMessage(question6(event));
-     }
-     else if(event.getMessageContent().contains(COMMAND7)) {
-         event.getChannel().sendMessage(question7(event));
-     }
-     else if(event.getMessageContent().contains(COMMAND8)) {
-         event.getChannel().sendMessage(question8(event));
-     }
-     else if(event.getMessageContent().contains(COMMAND9)) {
-         event.getChannel().sendMessage(question9(event));
-     }
+        else if(event.getMessageContent().contains(COMMAND4)) {
+            event.getChannel().sendMessage(d);
+        }
+        else if(event.getMessageContent().contains(COMMAND4A)){
+            question4(event);
+        }
+        else if(event.getMessageContent().contains(COMMAND5)) {
+            event.getChannel().sendMessage(e);
+        }
+        else if(event.getMessageContent().contains(COMMAND5A)) {
+            question5(event);
+        }
+        else if(event.getMessageContent().contains(COMMAND6)) {
+            event.getChannel().sendMessage(f);
+        }
+        else if(event.getMessageContent().contains(COMMAND6A)) {
+            question6(event);
+        }
+        else if(event.getMessageContent().contains(COMMAND7)) {
+            event.getChannel().sendMessage(g);
+        }
+        else if(event.getMessageContent().contains(COMMAND7A)){
+            question7(event);
+        }
+        else if(event.getMessageContent().contains(COMMAND8)) {
+            event.getChannel().sendMessage(h);
+        }
+        else if(event.getMessageContent().contains(COMMAND8A)) {
+            question8(event);
+        }
+        else if(event.getMessageContent().contains(COMMAND9)) {
+            event.getChannel().sendMessage(i);
+        }
+        else if(event.getMessageContent().contains(COMMAND9A)) {
+            question9(event);
+        }
      else if(event.getMessageContent().contains(COMMAND0)) {
-         event.getChannel().sendMessage(question0(event));
-     } else if(event.getMessageContent().contains(SCORE)){
+         event.getChannel().sendMessage(j);
+     }
+     else if(event.getMessageContent().contains(COMMAND0A)) {
+         question0(event);
+     }
+     else if(event.getMessageContent().contains(SCORE)){
          event.getChannel().sendMessage(String.valueOf(score));
      }
     }
-    public String question1 (MessageCreateEvent event) throws InterruptedException {
-        event.getChannel().sendMessage(a);
-
-        String messageContent = event.getMessageContent();
-        System.out.println(messageContent);
-        //String answer = messageContent.replaceAll(" ", "").replace(COMMAND1, "");
-
-
-        do {
-            System.out.println(messageContent);
-            if (messageContent.equals("!riddle1")){
-                qTimer.start();
-                answeredEvent=event;
+    public void question1 (MessageCreateEvent event) throws InterruptedException {
+        answeredEvent = event;
+            String messageContent = event.getMessageContent();
+            if (messageContent.contains(COMMAND1A)) {
+                System.out.println(messageContent.substring(9));
+                if (messageContent.substring(9).equals(a1)){
+                    score = score + 1;
+                    event.getChannel().sendMessage("Correct");
+                } else {
+                    score = score - 1;
+                    event.getChannel().sendMessage("Incorrect");
+                }
             }
-
-            if (messageContent.equalsIgnoreCase(a1)) {
+    }
+    public void question2 (MessageCreateEvent event) throws InterruptedException {
+        answeredEvent = event;
+        String messageContent = event.getMessageContent();
+        if (messageContent.contains(COMMAND2A)) {
+            System.out.println(messageContent.substring(9));
+            if (messageContent.substring(9).equals(b1)) {
                 score = score + 1;
-                return "Correct";
-
+                event.getChannel().sendMessage("Correct");
             } else {
                 score = score - 1;
-                return "Incorrect";
+                event.getChannel().sendMessage("Incorrect");
             }
-        } while(messageContent!="!riddle1");
-
-    }
-    public String question2 (MessageCreateEvent event) throws InterruptedException {
-        event.getChannel().sendMessage(b);
-        java.util.concurrent.TimeUnit.SECONDS.sleep (3);
-        String messageContent = event.getMessageContent();
-        String answer2 = messageContent.replaceAll(" ", "").replace(COMMAND2, "");
-        if (answer2.equals(b1)) {
-            score = score + 1;
-            return "Correct";
-
-        } else {
-            score = score-1;
-            return "Incorrect";
         }
     }
-    public String question3(MessageCreateEvent event) throws InterruptedException {
-        event.getChannel().sendMessage(c);
-        java.util.concurrent.TimeUnit.SECONDS.sleep (3);
+    public void question3(MessageCreateEvent event) throws InterruptedException {
+        answeredEvent = event;
         String messageContent = event.getMessageContent();
-        String answer3 = messageContent.replaceAll(" ", "").replace(COMMAND3, "");
-        if (answer3.equals(c1)) {
-            score = score + 1;
-            return "Correct";
-
-        } else {
-            score = score-1;
-            return "Incorrect";
-        }
-    }
-    public String question4 (MessageCreateEvent event) throws InterruptedException {
-        event.getChannel().sendMessage(d);
-        java.util.concurrent.TimeUnit.SECONDS.sleep (3);
-        String messageContent = event.getMessageContent();
-        String answer4 = messageContent.replaceAll(" ", "").replace(COMMAND4, "");
-        if (answer4.equals(d1)) {
-            score = score + 1;
-            return "Correct";
-
-        } else {
-            score = score-1;
-            return "Incorrect";
-        }
-    }
-    public String question5 (MessageCreateEvent event) throws InterruptedException {
-        event.getChannel().sendMessage(e);
-        java.util.concurrent.TimeUnit.SECONDS.sleep (3);
-        String messageContent = event.getMessageContent();
-        String answer5 = messageContent.replaceAll(" ", "").replace(COMMAND5, "");
-        if (answer5.equals(e1)) {
-            score = score + 1;
-            return "Correct";
-
-        } else {
-            score = score-1;
-            return "Incorrect";
-        }
-    }
-    public String question6 (MessageCreateEvent event) throws InterruptedException {
-        event.getChannel().sendMessage(f);
-        java.util.concurrent.TimeUnit.SECONDS.sleep (3);
-        String messageContent = event.getMessageContent();
-        String answer6 = messageContent.replaceAll(" ", "").replace(COMMAND6, "");
-        if (answer6.equals(f1)) {
-            score = score + 1;
-            return "Correct";
-
-        } else {
-            score = score-1;
-            return "Incorrect";
-        }
-    }
-    public String question7 (MessageCreateEvent event) throws InterruptedException {
-        event.getChannel().sendMessage(g);
-        java.util.concurrent.TimeUnit.SECONDS.sleep (3);
-        String messageContent = event.getMessageContent();
-        String answer7 = messageContent.replaceAll(" ", "").replace(COMMAND7, "");
-        if (answer7.equals(g1)) {
-            score = score + 1;
-            return "Correct";
-
-        } else {
-            score = score-1;
-            return "Incorrect";
-        }
-    }
-    public String question8 (MessageCreateEvent event) throws InterruptedException {
-        event.getChannel().sendMessage(h);
-
-        String messageContent = event.getMessageContent();
-        if (messageContent != " ") {
-            java.util.concurrent.TimeUnit.SECONDS.sleep(2);
-    } else{
-            String answer8 = messageContent.replaceAll(" ", "").replace(COMMAND8, "");
-            if (answer8.equals(h1)) {
+        if (messageContent.contains(COMMAND3A)) {
+            System.out.println(messageContent.substring(9));
+            if (messageContent.substring(9).equals(c1)){
                 score = score + 1;
-                return "Correct";
-
+                event.getChannel().sendMessage("Correct");
             } else {
                 score = score - 1;
-                return "Incorrect";
+                event.getChannel().sendMessage("Incorrect");
             }
         }
-        return "Incorrect";
     }
-    public String question9 (MessageCreateEvent event) throws InterruptedException {
-        event.getChannel().sendMessage(i);
-        java.util.concurrent.TimeUnit.SECONDS.sleep (8);
+    public void question4 (MessageCreateEvent event) throws InterruptedException {
+        answeredEvent = event;
         String messageContent = event.getMessageContent();
-        String answer9 = messageContent.replaceAll(" ", "").replace(COMMAND9, "");
-        if (answer9.equals(i1)) {
-            score = score + 1;
-            return "Correct";
-
-        } else {
-            score = score-1;
-            return "Incorrect";
+        if (messageContent.contains(COMMAND4A)) {
+            System.out.println(messageContent.substring(9));
+            if (messageContent.substring(9).equals(d1)){
+                score = score + 1;
+                event.getChannel().sendMessage("Correct");
+            } else {
+                score = score - 1;
+                event.getChannel().sendMessage("Incorrect");
+            }
         }
     }
-    public String question0 (MessageCreateEvent event) throws InterruptedException {
-        event.getChannel().sendMessage(j);
-        java.util.concurrent.TimeUnit.SECONDS.sleep (8);
+    public void question5 (MessageCreateEvent event) throws InterruptedException {
+        answeredEvent = event;
         String messageContent = event.getMessageContent();
-        String answer0 = messageContent.replaceAll(" ", "").replace(COMMAND0, "");
-        if (answer0.equals(j1)) {
-            score = score + 1;
-            return "Correct";
-
-        } else {
-            score = score-1;
-            return "Incorrect";
+        if (messageContent.contains(COMMAND5A)) {
+            System.out.println(messageContent.substring(9));
+            if (messageContent.substring(9).equals(e1)){
+                score = score + 1;
+                event.getChannel().sendMessage("Correct");
+            } else {
+                score = score - 1;
+                event.getChannel().sendMessage("Incorrect");
+            }
+        }
+    }
+    public void question6 (MessageCreateEvent event) throws InterruptedException {
+        answeredEvent = event;
+        String messageContent = event.getMessageContent();
+        if (messageContent.contains(COMMAND6A)) {
+            System.out.println(messageContent.substring(9));
+            if (messageContent.substring(9).equals(f1)){
+                score = score + 1;
+                event.getChannel().sendMessage("Correct");
+            } else {
+                score = score - 1;
+                event.getChannel().sendMessage("Incorrect");
+            }
+        }
+    }
+    public void question7 (MessageCreateEvent event) throws InterruptedException {
+        answeredEvent = event;
+        String messageContent = event.getMessageContent();
+        if (messageContent.contains(COMMAND7A)) {
+            System.out.println(messageContent.substring(9));
+            if (messageContent.substring(9).equals(g1)){
+                score = score + 1;
+                event.getChannel().sendMessage("Correct");
+            } else {
+                score = score - 1;
+                event.getChannel().sendMessage("Incorrect");
+            }
+        }
+    }
+    public void question8 (MessageCreateEvent event) throws InterruptedException {
+        answeredEvent = event;
+        String messageContent = event.getMessageContent();
+        if (messageContent.contains(COMMAND8A)) {
+            System.out.println(messageContent.substring(9));
+            if (messageContent.substring(9).equals(h1)){
+                score = score + 1;
+                event.getChannel().sendMessage("Correct");
+            } else {
+                score = score - 1;
+                event.getChannel().sendMessage("Incorrect");
+            }
+        }
+    }
+    public void question9 (MessageCreateEvent event) throws InterruptedException {
+        answeredEvent = event;
+        String messageContent = event.getMessageContent();
+        if (messageContent.contains(COMMAND9A)) {
+            System.out.println(messageContent.substring(9));
+            if (messageContent.substring(9).equals(i1)){
+                score = score + 1;
+                event.getChannel().sendMessage("Correct");
+            } else {
+                score = score - 1;
+                event.getChannel().sendMessage("Incorrect");
+            }
+        }
+    }
+    public void question0 (MessageCreateEvent event) throws InterruptedException {
+        answeredEvent = event;
+        String messageContent = event.getMessageContent();
+        if (messageContent.contains(COMMAND0A)) {
+            System.out.println(messageContent.substring(9));
+            if (messageContent.substring(9).equals(j1)){
+                score = score + 1;
+                event.getChannel().sendMessage("Correct");
+            } else {
+                score = score - 1;
+                event.getChannel().sendMessage("Incorrect");
+            }
         }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        qTimer.stop();
             answeredEvent.getChannel().sendMessage("You ran out of time");
 
 
