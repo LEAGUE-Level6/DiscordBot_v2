3a228078332fa3b33c542b1bb18f4639c375af03
cfredberg <charliefredberg@outlook.com>
2023-03-20 16:57:42 -0700

    tried to work on getting responses from api.  wrong places don't work

--- src/main/java/org/jointheleague/features/student/Weather.java
+++ src/main/java/org/jointheleague/features/student/Weather.java
@@ -4,9 +4,12 @@ import org.javacord.api.event.message.MessageCreateEvent;
 import org.jointheleague.features.abstract_classes.Feature;
 import org.jointheleague.features.help_embed.plain_old_java_objects.help_embed.HelpEmbed;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.util.Optional;
 
 public class Weather extends Feature {
 
@@ -37,25 +40,32 @@ public class Weather extends Feature {
         String messageContent = event.getMessageContent();
         if (messageContent.startsWith(COMMAND)) {
             //respond to message here
+            String location = messageContent.substring(8).trim();
+            event.getChannel().sendMessage(location);
             event.getChannel().sendMessage("Testing...");
-            event.getChannel().sendMessage(requestWeather());
+            String data = requestWeather(location);
+            System.out.println(data);
             event.getChannel().sendMessage("Working");
         }
     }
 
-    public String requestWeather(){
+    public String requestWeather(String location){
         Mono<String> stringMono = webClient
                 .get()
                 .uri(uriBuilder -> uriBuilder
                         .queryParam("key", "1bbb25a126c4416abc8190539231903")
-                        .queryParam("q", "San Diego")
+                        .queryParam("q", location)
                         .build())
                 .retrieve()
                 .bodyToMono(String.class);
-
-        System.out.println("test");
-
+        System.out.println("hhhhhhhhhh");
+        Optional<String> result = stringMono.blockOptional();
+        if (result.equals(Optional.empty())){
+            return "Error";
+        }
+        System.out.println("passed");
         String response = stringMono.block();
+        System.out.println("hello");
 
         return response;
     }
