0a247f9bddb66474c3bf2da616ff730aa5ae13fe
Mike1000e <56172395+Mike1000e@users.noreply.github.com>
2023-06-09 17:38:05 -0700

    getting started done

--- src/main/java/org/jointheleague/discord_bot/DiscordBot.java
+++ src/main/java/org/jointheleague/discord_bot/DiscordBot.java
@@ -2,6 +2,7 @@ package org.jointheleague.discord_bot;
 
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.intent.Intent;
 import org.jointheleague.features.abstract_classes.Feature;
 import org.jointheleague.features.examples.second_features.HighLowGame;
 import org.jointheleague.features.examples.third_features.CatFactsApi;
@@ -9,6 +10,7 @@ import org.jointheleague.features.examples.third_features.NewsApi;
 import org.jointheleague.features.examples.first_features.CurrentTime;
 import org.jointheleague.features.examples.first_features.RandomNumber;
 import org.jointheleague.features.help_embed.HelpListener;
+import org.jointheleague.features.student.RockPaperScissors;
 
 
 //delete
@@ -31,7 +33,7 @@ public class DiscordBot {
 	public void connect(boolean printInvite) {
 
 
-		api = new DiscordApiBuilder().setToken(token).login().join();
+		api = new DiscordApiBuilder().addIntents(Intent.MESSAGE_CONTENT).setToken(token).login().join();
 
 		//Print the URL to invite the bot
 		if (printInvite) {
@@ -46,6 +48,7 @@ public class DiscordBot {
 		api.addMessageCreateListener(helpListener);
 
 		//add features
+		addFeature(new RockPaperScissors(channelName));
 		addFeature(new RandomNumber(channelName));
 		addFeature(new CurrentTime(channelName));
 		addFeature(new HighLowGame(channelName));
