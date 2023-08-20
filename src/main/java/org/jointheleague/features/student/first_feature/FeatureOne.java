f3a992617ea2982896b9028c9c0e27f4592d82df
Branson778 <bransonator123@gmail.com>
2023-08-20 16:31:00 -0700

    Finish Unit Testing for Feature 1

---
+++ src/main/java/org/jointheleague/features/student/first_feature/FeatureOne.java
@@ -0,0 +1,47 @@
+package org.jointheleague.features.student.first_feature;
+
+import org.javacord.api.event.message.MessageCreateEvent;
+import org.jointheleague.features.help_embed.plain_old_java_objects.help_embed.HelpEmbed;
+import org.jointheleague.features.abstract_classes.Feature;
+
+import java.util.Random;
+
+public class FeatureOne extends Feature {
+    public final String COMMAND = "!joke";
+
+    public FeatureOne(String channelName) {
+        super(channelName);
+
+        //Create a help embed to describe feature when !help command is sent
+        helpEmbed = new HelpEmbed(
+                COMMAND,
+                "This command prints cheesy jokes. Remember to make unit tests for this."
+        );
+    }
+//IGNORE
+    @Override
+    public void handle(MessageCreateEvent event) {
+        String messageContent = event.getMessageContent();
+        if (messageContent.startsWith(COMMAND)) {
+            //respond to message here
+            Random ran = new Random();
+            int ranInt = ran.nextInt(5);
+
+            switch(ranInt){
+                case 0:event.getChannel().sendMessage("What’s the best thing about Switzerland? \n \n I don’t know, but the flag is a big plus.");
+                break;
+                case 1:event.getChannel().sendMessage("I invented a new word! \n \n Plagiarism!");
+                break;
+                case 2:event.getChannel().sendMessage("Hear about the new restaurant called Karma? \n \n There’s no menu: You get what you deserve.");
+                    break;
+                case 3:event.getChannel().sendMessage("Did you hear about the claustrophobic astronaut? \n \n He just needed a little space.");
+                    break;
+                case 4:event.getChannel().sendMessage("Why don’t scientists trust atoms? \n \n Because they make up everything.");
+                    break;
+                default:event.getChannel().sendMessage("What’s the different between a cat and a comma? \n \n A cat has claws at the end of paws; A comma is a pause at the end of a clause. ");
+                    break;
+            }
+
+        }
+    }
+}
