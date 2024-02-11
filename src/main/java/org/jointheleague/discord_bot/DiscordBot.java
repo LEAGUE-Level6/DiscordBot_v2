1f9c0cb217d6204f13c5c78570246b6f31a8e0c6
Aarit Jayaswal <aaritrj@gmail.com>
2024-02-11 11:27:41 -0800

    Worked on discord bot

--- src/main/java/org/jointheleague/discord_bot/DiscordBot.java
+++ src/main/java/org/jointheleague/discord_bot/DiscordBot.java
@@ -11,6 +11,7 @@ import org.jointheleague.features.examples.first_features.CurrentTime;
 import org.jointheleague.features.examples.first_features.RandomNumber;
 import org.jointheleague.features.help_embed.HelpListener;
 import org.jointheleague.features.student.first_feature.FeatureOne;
+import org.jointheleague.features.student.first_feature.Trivia;
 
 public class DiscordBot {
 
@@ -53,6 +54,7 @@ public class DiscordBot {
 		addFeature(new HighLowGame(channelName));
 		addFeature(new NewsApi(channelName));
 		addFeature(new CatFactsApi(channelName));
+		addFeature(new Trivia(channelName));
 	}
 
 	private void addFeature(Feature feature){
