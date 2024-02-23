8718d9a903a7de84d2a17f1e9886dd84eca5d936
league <powerrspyy@gmail.com>
2024-02-22 18:28:04 -0800

    Tests for Feature Two done

---
+++ src/test/java/org/jointheleague/features/student/second_feature/FeatureTwoTest.java
@@ -0,0 +1,139 @@
+package org.jointheleague.features.student.second_feature;
+
+import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.event.message.MessageCreateEvent;
+import org.jointheleague.features.help_embed.plain_old_java_objects.help_embed.HelpEmbed;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.never;
+
+public class FeatureTwoTest {
+    private final String testChannelName = "test";
+    private final FeatureTwo featTwo = new FeatureTwo(testChannelName);
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Mock
+    private MessageCreateEvent messageCreateEvent;
+
+    @Mock
+    private TextChannel textChannel;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void itShouldNotPrintToSystemOut() {
+        String expected = "";
+        String actual = outContent.toString();
+
+        assertEquals(expected, actual);
+        System.out.println(actual);
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void itShouldHaveACommand() {
+        //Given
+
+        //When
+        String command = featTwo.COMMAND;
+
+        //Then
+        assertNotEquals("", command);
+        assertNotEquals("!command", command);
+        assertNotNull(command);
+    }
+
+    @Test
+    void itShouldHandleMessagesWithCommand() {
+        //Given
+        HelpEmbed helpEmbed = new HelpEmbed(featTwo.COMMAND, "test");
+        when(messageCreateEvent.getMessageContent()).thenReturn(featTwo.COMMAND);
+        when(messageCreateEvent.getChannel()).thenReturn((textChannel));
+
+        //When
+        featTwo.handle(messageCreateEvent);
+
+        //Then
+        verify(textChannel, times(1)).sendMessage(anyString());
+    }
+
+    @Test
+    void itShouldNotHandleMessagesWithoutCommand() {
+        //Given
+        String command = "";
+        when(messageCreateEvent.getMessageContent()).thenReturn(command);
+
+        //When
+        featTwo.handle(messageCreateEvent);
+
+        //Then
+        verify(textChannel, never()).sendMessage("");
+    }
+
+    @Test
+    void itShouldHaveAHelpEmbed() {
+        //Given
+
+        //When
+        HelpEmbed actualHelpEmbed = featTwo.getHelpEmbed();
+
+        //Then
+        assertNotNull(actualHelpEmbed);
+    }
+
+    @Test
+    void itShouldHaveTheCommandAsTheTitleOfTheHelpEmbed() {
+        //Given
+
+        //When
+        String helpEmbedTitle = featTwo.getHelpEmbed().getTitle();
+        String command = featTwo.COMMAND;
+
+        //Then
+        assertEquals(command, helpEmbedTitle);
+    }
+
+    @Test
+    void itShouldSayIfYouAreCorrect() {
+        String guess = "4";
+        featTwo.currentQ.setAnswer("4");
+        featTwo.gameStart = 1;
+        when(messageCreateEvent.getMessageContent()).thenReturn(guess);
+        when(messageCreateEvent.getChannel()).thenReturn((textChannel));
+
+        featTwo.handle(messageCreateEvent);
+
+        verify(textChannel, times(1)).sendMessage("CORRECT!!! Question 2 will begin shortly...");
+    }
+
+    @Test
+    void itShouldSayIfYouAreIncorrect() {
+        String guess = "8";
+        featTwo.currentQ.setAnswer("4");
+        featTwo.gameStart = 1;
+        when(messageCreateEvent.getMessageContent()).thenReturn(guess);
+        when(messageCreateEvent.getChannel()).thenReturn((textChannel));
+
+        featTwo.handle(messageCreateEvent);
+
+        verify(textChannel, times(1)).sendMessage("Incorrect! Try again");
+    }
+}
