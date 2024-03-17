781481642e8f15462b18620c547fadf2ea79aab7
hailinday <54258211+hailinday@users.noreply.github.com>
2024-03-17 13:00:53 -0700

    change direction

--- src/main/java/org/jointheleague/features/student/third_feature/ApiExample.java
+++ src/main/java/org/jointheleague/features/student/third_feature/ApiExample.java
@@ -1,87 +1,31 @@
-/**
- * Sample Java code for youtube.search.list
- * See instructions for running these code samples locally:
- * https://developers.google.com/explorer-help/code-samples#java
- */
+package org.jointheleague.features.student.third_feature;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.googleapis.json.GoogleJsonResponseException;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-
-import com.google.api.services.youtube.YouTube;
-import com.google.api.services.youtube.model.SearchListResponse;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.security.GeneralSecurityException;
-import java.util.Arrays;
-import java.util.Collection;
+import org.springframework.web.reactive.function.client.WebClient;
 
 public class ApiExample {
-    private static final String CLIENT_SECRETS= "client_secret.json";
-    private static final Collection<String> SCOPES =
-            Arrays.asList("https://www.googleapis.com/auth/youtube.force-ssl");
+member variables:
+private WebClient webClient;
+private static final String baseUrl = "numbersapi.com/";
 
-    private static final String APPLICATION_NAME = "API code samples";
-    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-
-    /**
-     * Create an authorized Credential object.
-     *
-     * @return an authorized Credential object.
-     * @throws IOException
-     */
-    public static Credential authorize(final NetHttpTransport httpTransport) throws IOException {
-        // Load client secrets.
-        InputStream in = ApiExample.class.getResourceAsStream(CLIENT_SECRETS);
-        GoogleClientSecrets clientSecrets =
-                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow =
-                new GoogleAuthorizationCodeFlow.Builder(httpTransport, JSON_FACTORY, clientSecrets, SCOPES)
-                        .build();
-        Credential credential =
-                new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
-        return credential;
-    }
-
-    /**
-     * Build and return an authorized API client service.
-     *
-     * @return an authorized API client service
-     * @throws GeneralSecurityException, IOException
-     */
-    public static YouTube getService() throws GeneralSecurityException, IOException {
-        final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-        Credential credential = authorize(httpTransport);
-        return new YouTube.Builder(httpTransport, JSON_FACTORY, credential)
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-    }
-
-    /**
-     * Call function to create API service object. Define and
-     * execute API request. Print API response.
-     *
-     * @throws GeneralSecurityException, IOException, GoogleJsonResponseException
-     */
-    public static void main(String[] args)
-            throws GeneralSecurityException, IOException, GoogleJsonResponseException {
-        YouTube youtubeService = getService();
-        // Define and execute the API request
-        YouTube.Search.List request = youtubeService.search()
-                .list("snippet");
-        SearchListResponse response = request.setMaxResults(25L)
-                .setQ("surfing")
-                .execute();
-        System.out.println(response);
-    }
+public ApiExample() {
+    this.webClient = WebClient
+            .builder()
+            .baseUrl(baseUrl)
+            .build();
 }
+public String[] getThing() {
+    Wrapper thing = webClient.get()
+            .uri(uriBuilder -> uriBuilder
+                    .queryParam(“your parameters”)
+                    .build())
+            .retrieve()
+            .bodyToMono(Wrapper.class)
+            .block();
+
+    return {//whatever you’re returning};
+
+
+    public void setWebClient(WebClient webClient) {
+        this.webClient = webClient;
+    }
+    }
