74472b5b741ae600008d48b698de2aa093c1fd0c
league <hailinday@gmail.com>
2024-04-18 18:28:05 -0700

    yay finished. now testing hell and more feats

--- src/main/java/org/jointheleague/features/student/third_feature/ApiExample.java
+++ src/main/java/org/jointheleague/features/student/third_feature/ApiExample.java
@@ -1,40 +1,37 @@
 package org.jointheleague.features.student.third_feature;
 
 import org.jointheleague.features.student.third_feature.data_transfer_objects.NumFact;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 public class ApiExample {
 
 private WebClient webClient;
-private static final String baseUrl = "numbersapi.com";
+private static final String baseUrl = "http://numbersapi.com";
 
 public ApiExample() {
     this.webClient = WebClient
             .builder()
             .baseUrl(baseUrl)
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
             .build();
 }
 public NumFact getNumFact(String number) {
-    System.out.println("Test!!");
     Mono<NumFact> numMono = webClient.get()
-            .uri(uriBuilder -> uriBuilder
-                    .path("/" + number + "?json")
-                    .build())
+            .uri("/" + number)
             .retrieve()
             .bodyToMono(NumFact.class);
-    System.out.println("Test :(");
     return numMono.block();
 }
 public String findNumFact(String numberPick) {
-    System.out.println(numberPick);
     NumFact numWrap = getNumFact(numberPick);
     String numFact = numWrap.getText();
-    System.out.println(numFact + "TEST");
     return numFact;
 }
 
-    public void setWebClient(WebClient webClient) {
-        this.webClient = webClient;
-    }
+public void setWebClient(WebClient webClient) {
+    this.webClient = webClient;
+}
     }
