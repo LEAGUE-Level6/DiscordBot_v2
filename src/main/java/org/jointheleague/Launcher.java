ce230635d7d7b6b9f94748c5f7be105ff56d6edb
lvl0_module2 <aaritrj@gmail.com>
2024-09-12 18:01:53 -0700

    :b:

--- src/main/java/org/jointheleague/Launcher.java
+++ src/main/java/org/jointheleague/Launcher.java
@@ -7,8 +7,8 @@ public class Launcher {
     public static void main(String[] args) throws InterruptedException {
 
         //Initialize variables
-        String channelName = "";
-        String discordToken = "";
+        String channelName = "eddie";
+        String discordToken = "MTI2MTEyMjg0MTk0MDcyNTg3NQ.GoMBCe.Noh6EkpP7UnKB__QSNWPzTL33NHozv_2YnBQSE";
         boolean printDiscordInvite = true;
 
         //Instantiate DiscordBot and connect
