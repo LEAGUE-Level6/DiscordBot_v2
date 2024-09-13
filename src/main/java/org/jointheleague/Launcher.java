edaee483862e2aa57dee80b3182e55a59e595b9b
lvl0_module2 <aaritrj@gmail.com>
2024-09-12 17:45:58 -0700

    :b: this should work
    hopefull this saves

--- src/main/java/org/jointheleague/Launcher.java
+++ src/main/java/org/jointheleague/Launcher.java
@@ -7,7 +7,7 @@ public class Launcher {
 
         //Initialize variables
         String channelName = "eddie";
-        String discordToken = "";
+        String discordToken = "MTI2MTEyMjg0MTk0MDcyNTg3NQ.GoMBCe.Noh6EkpP7UnKB__QSNWPzTL33NHozv_2YnBQSE";
         boolean printDiscordInvite = true;
 
         //Instantiate DiscordBot and connect
--- src/main/java/org/jointheleague/features/student/ed_feature/edFeature.java
+++ src/main/java/org/jointheleague/features/student/ed_feature/edFeature.java
@@ -1,20 +1,40 @@
 package org.jointheleague.features.student.ed_feature;
 
+import org.jointheleague.api_wrapper.ReceivedMessage;
 import org.jointheleague.features.abstract_classes.Feature;
+import org.jointheleague.features.help_embed.plain_old_java_objects.help_embed.HelpEmbed;
+
+import java.util.Random;
+
 
 public class edFeature extends Feature{
+    Random rand = new Random();
     public final String COMMAND = "!edFeature";
 
     public edFeature (String channelName){
         super(channelName);
-        helpEmbed = new helpEmbed(COMMAND, "This is Eddie's Feature. He hasn't decided what it is yet but it should at least do something.");
+        helpEmbed = new HelpEmbed(COMMAND, "This is Eddie's Feature. He hasn't decided what it is yet but it should at least do something.");
     }
 
-    public void handle(RecievedMessage event){
+    @Override
+    public void handle(ReceivedMessage event) {
         String messageContent = event.getMessageContent();
 
         if( messageContent.contains(COMMAND)){
             event.sendResponse("yup! Eddie's Feature can do stuff!");
         }
     }
-}
+
+    private String rollFeature(String inputRoll){
+        inputRoll.replace(COMMAND, "");
+        String finalTotal = "";
+        String[] dice = inputRoll.split("[+-]");
+        for(String nums : dice){
+                String[] toRoll = nums.toLowerCase().split("d");
+                finalTotal += nums + " rolled: " + rand.nextInt(Integer.parseInt(toRoll[1])) + "\n";
+                
+           }
+        return finalTotal;
+    }
+
+    }
