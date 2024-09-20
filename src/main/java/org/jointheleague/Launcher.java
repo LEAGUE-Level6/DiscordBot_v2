1cda18b57817626f4c8061ef7e00eaabc09a392c
lvl0_module2 <Eddiejhunter1000000@gmail.com>
2024-09-19 18:35:12 -0700

    :b:

--- src/main/java/org/jointheleague/Launcher.java
+++ src/main/java/org/jointheleague/Launcher.java
@@ -8,7 +8,7 @@ public class Launcher {
 
         //Initialize variables
         String channelName = "eddie";
-        String discordToken = "MTI2MTEyMjg0MTk0MDcyNTg3NQ.GHpPqV.uLpiyVmXAGFc5Jw44oXTmET1MRxdPZnuwKzoQ4";
+        String discordToken = "";
         boolean printDiscordInvite = true;
 
         //Instantiate DiscordBot and connect
