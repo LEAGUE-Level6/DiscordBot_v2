4bc18f2a90cc583d2a7a7db26c18357f13b1b017
YeetMaster467 <elicomixinc@gmail.com>
2025-02-26 19:31:30 -0800

    Added second feature, chill command.

--- src/main/java/org/jointheleague/discord_bot/DiscordBot.java
+++ src/main/java/org/jointheleague/discord_bot/DiscordBot.java
@@ -14,6 +14,7 @@ import org.jointheleague.features.examples.first_features.CurrentTime;
 import org.jointheleague.features.examples.first_features.RandomNumber;
 import org.jointheleague.features.help_embed.HelpListener;
 import org.jointheleague.features.student.first_feature.FeatureOne;
+import org.jointheleague.features.student.first_feature.FeatureTwo;
 
 public class DiscordBot {
 
@@ -61,6 +62,7 @@ public class DiscordBot {
 		addFeature(new HighLowGame(channelName));
 		addFeature(new NewsApi(channelName));
 		addFeature(new CatFactsApi(channelName));
+		addFeature(new FeatureTwo(channelName));
 	}
 
 	private void addFeature(Feature feature){
