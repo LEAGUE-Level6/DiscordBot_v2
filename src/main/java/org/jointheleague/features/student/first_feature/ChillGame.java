14ed314deac8743a23587a4e9b6d508a4a49ec6c
YeetMaster467 <elicomixinc@gmail.com>
2025-02-26 19:58:43 -0800

    Started on chill game, big command that spans multiple messages.

---
+++ src/main/java/org/jointheleague/features/student/first_feature/ChillGame.java
@@ -0,0 +1,30 @@
+package org.jointheleague.features.student.first_feature;
+
+import org.jointheleague.api_wrapper.ReceivedMessage;
+import org.jointheleague.features.abstract_classes.Feature;
+import org.jointheleague.features.help_embed.plain_old_java_objects.help_embed.HelpEmbed;
+
+public class ChillGame extends Feature {
+
+    public final String COMMAND = "!command";
+
+    public ChillGame(String channelName) {
+        super(channelName);
+
+        //Create a help embed to describe feature when !help command is sent
+        helpEmbed = new HelpEmbed(
+                COMMAND,
+                "This is a pretty chill game, that's fun to play. It'll be pretty chill."
+        );
+    }
+
+    @Override
+    public void handle(ReceivedMessage event) {
+        String messageContent = event.getMessageContent();
+        if (messageContent.startsWith(COMMAND)) {
+            //respond to message here
+            event.sendResponse("Sending a message to the channel");
+        }
+    }
+
+}
