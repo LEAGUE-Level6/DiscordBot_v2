1b2e210528202d28356f4383b21441496ec527f1
YeetMaster467 <elicomixinc@gmail.com>
2025-03-22 11:27:09 -0700

    Started on the game mechanics, added a format for the command.

--- src/main/java/org/jointheleague/features/student/first_feature/ChillGame.java
+++ src/main/java/org/jointheleague/features/student/first_feature/ChillGame.java
@@ -16,7 +16,7 @@ public class ChillGame extends Feature {
         //Create a help embed to describe feature when !help command is sent
         helpEmbed = new HelpEmbed(
                 COMMAND,
-                "This is a pretty chill game, that's fun to play. It'll be pretty chill."
+                "Literally just hangman"
         );
     }
 
@@ -27,9 +27,47 @@ public class ChillGame extends Feature {
             //respond to message here
             if (messageContent.equals(COMMAND)){
                 wordToGuess = Utilities.readRandomLineFromFile("src/main/java/org/jointheleague/features/student/first_feature/dictionary.txt");
+                for (int i = 0; i < wordToGuess.length(); i++){
+                    currentDisplay += "_";
+                }
                 event.sendResponse("There is a word you need to guess, guess a letter by doing the command, then a letter. Ex: \"!chillGame e\"" + "\nDEBUG: " + wordToGuess);
                 System.out.println(wordToGuess);
             } else {
+                //check if the game has been started
+                if(wordToGuess.isEmpty()){
+                    //tell them to start the game first
+                    event.sendResponse("Please start the game first using just the command");
+                    return;
+                }
+
+                //parse the guess from the message
+                String guessMessage = messageContent.replaceAll(" ", "").replace(COMMAND, "");
+
+                //change the guess to a char
+               if (!guessMessage.equalsIgnoreCase(wordToGuess)){
+                   char guess = 0;
+                   try{
+                       guess = guessMessage.charAt(0);
+                   }
+                   catch(IndexOutOfBoundsException e){
+                       //tell them to format their guess properly
+                       event.sendResponse("Please format your guess like this: " + COMMAND + " e");
+                       return;
+                   }
+
+                   if (wordToGuess.contains(guess + "") && currentDisplay.contains(guess + "")){
+
+                   } else {
+                       event.sendResponse("Incorrect Guess!!\n" + currentDisplay);
+                   }
+
+               } else {
+                    //they got it correct
+                    event.sendResponse("Correct!  The word I picked was " + wordToGuess);
+                    //set wordToGuess back to empty string
+                    wordToGuess = "";
+               }
+
 
             }
         }
