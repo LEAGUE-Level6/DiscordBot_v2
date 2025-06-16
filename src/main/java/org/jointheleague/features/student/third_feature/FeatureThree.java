46da33b7d28b7d411f8e11dc5572c0862db12163
opie10 <augie@keatinge.com>
2025-06-16 16:58:51 -0700

    getting there choosing an api maybe lotr?

---
+++ src/main/java/org/jointheleague/features/student/third_feature/FeatureThree.java
@@ -0,0 +1,30 @@
+package org.jointheleague.features.student.third_feature;
+
+import org.jointheleague.api_wrapper.ReceivedMessage;
+import org.jointheleague.features.abstract_classes.Feature;
+import org.jointheleague.features.help_embed.plain_old_java_objects.help_embed.HelpEmbed;
+
+public class FeatureThree extends Feature {
+
+    public final String COMMAND = "!command";
+
+    public FeatureThree(String channelName) {
+        super(channelName);
+
+        //Create a help embed to describe feature when !help command is sent
+        helpEmbed = new HelpEmbed(
+                COMMAND,
+                "Give a brief description of your feature here, including how the user interacts with it"
+        );
+    }
+
+    @Override
+    public void handle(ReceivedMessage event) {
+        String messageContent = event.getMessageContent();
+        if (messageContent.startsWith(COMMAND)) {
+            //respond to message here
+            event.sendResponse("Sending a message to the channel");
+        }
+    }
+
+}
