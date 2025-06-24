58cf4790cf9759528c78d95de536e382db844d0a
opie10 <augie@keatinge.com>
2025-06-23 17:01:34 -0700

    alright its getting there. I can now get image links from the NASA API, issue appears when sending them. created embed with no errors, but no images actually appear? make sure sizes are right?

--- src/main/java/org/jointheleague/features/student/third_feature/FeatureThree.java
+++ src/main/java/org/jointheleague/features/student/third_feature/FeatureThree.java
@@ -1,30 +1,62 @@
 package org.jointheleague.features.student.third_feature;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import org.jointheleague.api_wrapper.ReceivedMessage;
 import org.jointheleague.features.abstract_classes.Feature;
+import org.jointheleague.features.examples.third_features.plain_old_java_objects.cat_facts_api.CatWrapper;
 import org.jointheleague.features.help_embed.plain_old_java_objects.help_embed.HelpEmbed;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.awt.*;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
 
 public class FeatureThree extends Feature {
 
-    public final String COMMAND = "!command";
-
+    public final String COMMAND = "!SpacePic";
+    private WebClient webClient;
+    private static final String baseUrl = "https://api.nasa.gov/planetary/apod?api_key="+System.getenv("API_KEY");
     public FeatureThree(String channelName) {
         super(channelName);
 
         //Create a help embed to describe feature when !help command is sent
         helpEmbed = new HelpEmbed(
                 COMMAND,
-                "Give a brief description of your feature here, including how the user interacts with it"
+                "Provides the picture of the day from the NASA APOD API by default. Add"
         );
+        this.webClient = WebClient
+                .builder()
+                .baseUrl(baseUrl)
+                .build();
     }
 
     @Override
     public void handle(ReceivedMessage event) {
+
         String messageContent = event.getMessageContent();
+        System.out.println("Message Received");
+        System.out.println(messageContent);
         if (messageContent.startsWith(COMMAND)) {
             //respond to message here
-            event.sendResponse("Sending a message to the channel");
+            System.out.println("Printing Image");
+            String imageLink = getAPOD();
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setImage(imageLink);
+            event.sendResponse(embed.build());
         }
+    }
+    public String getAPOD(){
+        System.out.println("Getting Image");
+        Mono<ImageWrapper> imageWrapperMono = webClient.get()
+                .retrieve()
+                .bodyToMono(ImageWrapper.class);
+
+        //collect the response into a plain old java object
+        ImageWrapper imageWrapper = imageWrapperMono.block();
+        String image = imageWrapper.getUrl();
+        System.out.println(image);
+        return image;
     }
 
 }
